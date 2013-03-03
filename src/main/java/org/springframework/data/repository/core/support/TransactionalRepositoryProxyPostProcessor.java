f84fe60c054706b4d15ca27812c88f88b573dcf1
Oliver Gierke <info@olivergierke.de>
2013-03-03 15:29:24 +0100

    DATACMNS-293 - Basic infrastructure for query augmentation.
    Added SPI to support generic query augmentation. Implementors implement a QueryAugmentor that can opt into augmentation and will get called for query executions and updates to potentially massage the query to be executed, manipulate or even replace the execution of updates.
    
    We expose a QueryAugmentationEngineAware injection interface for repository base class implementations to expose they want to take part in the query augmentation.
    
    TODO:
    - Add BeanPostProcessor to inject QueryAugmentationEngines into QueryAugmentationEngineAware classes
    - Automatically pick up QueryAugmentor instances from the application context
    - Ordering of QueryAugmentors
    - More tests

--- src/main/java/org/springframework/data/repository/core/support/TransactionalRepositoryProxyPostProcessor.java
+++ src/main/java/org/springframework/data/repository/core/support/TransactionalRepositoryProxyPostProcessor.java
@@ -79,7 +79,7 @@ class TransactionalRepositoryProxyPostProcessor implements RepositoryProxyPostPr
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.springframework.data.repository.core.support.RepositoryProxyPostProcessor#postProcess(org.springframework.aop.framework.ProxyFactory, org.springframework.data.repository.core.RepositoryInformation)
+	 * @see org.springframework.data.repository.core.support.RepositoryProxyPostProcessor#postProcess(org.springframework.aop.framework.ProxyFactory)
 	 */
 	public void postProcess(ProxyFactory factory, RepositoryInformation repositoryInformation) {
 
